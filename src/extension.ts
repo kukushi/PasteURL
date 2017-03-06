58cfe02c70b62bec98afdde6cd39b90eb74428b3
kukushi <17washabi@gmail.com>
2017-03-06 19:15:26 +0800

    Update configuration and add alert

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,21 @@ import * as vscode from 'vscode';
 import * as copyPaste from "copy-paste";
 
 var isURL = require('is-url');
-var articleTitle = require('article-title')
 var request = require('request');
+
+var showError = vscode.window.showInformationMessage;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "pasteurl" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
+    let disposable = vscode.commands.registerCommand('extension.pasteURL', () => {
         Paster.paste()
-        vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
@@ -34,7 +29,10 @@ class Paster {
     public static paste() {
         copyPaste.paste((error, content) => {
             if (content && isURL(content)) {
+                vscode.window.showInformationMessage('Geting title from URL!');
                 generateMarkDownStyleLink(content)
+            } else {
+                showError('Not a URL!');
             }
         })
     }
@@ -48,8 +46,14 @@ function generateMarkDownStyleLink(url) {
             var re =  /<title.*?>\s*(.*?)\s*<\/title/g;
             var match = re.exec(body)
             title = match[1]
-            var result = '[' + title + ']' + '(' + url + ')'
-            writeToEditor(result)
+            if (match.length > 1) {
+                var result = '[' + title + ']' + '(' + url + ')'
+                writeToEditor(result)
+            } else {
+                showError('Sorry, title not found!')
+            }
+        } else {
+            showError('Not a URL');
         }
      }
 
@@ -62,7 +66,6 @@ function writeToEditor(content) {
         let lastCharIndex = vscode.window.activeTextEditor.document.lineAt(startLine).text.length;
         let position = new vscode.Position(startLine, lastCharIndex);
         editBuilder.insert(position, content);
-
     });
 }
 
