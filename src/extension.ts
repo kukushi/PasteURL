f12b9308384b7fc342088fe9b58a22b859b85df0
kukushi <17washabi@gmail.com>
2017-03-22 23:49:33 +0800

    Use new interation when fetching title: work in progress

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { window, StatusBarItem, StatusBarAlignment } from 'vscode';
+import { window, StatusBarItem, StatusBarAlignment, Position, Range } from 'vscode';
 import * as vscode from 'vscode';
 import * as copyPaste from 'copy-paste';
 import * as getTitle from 'get-title';
@@ -39,19 +39,22 @@ export class Paster {
         var headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0.2 Safari/602.3.12"
         }
-        if (!url.startsWith("http")) {
+        if (!url.startsWith("http:")) {
             url = "http://" + url
         }
+
+        var fetchingTitle = 'Fetching Title...'
+        _this.writeToEditor('[' + fetchingTitle + '](' + url + ')')
+
         const stream = hyperquest(url, { headers: headers }, function (err, response) {
             if (err) {
-                _this.showMessage('Error happened when fetching title')
+                _this.replaceWith(fetchingTitle, 'Error Happened')
             }
         })
 
         getTitle(stream).then(title => {
-            title = this.processTitle(title, url)
-            var result = '[' + title + ']' + '(' + url + ')'
-            this.writeToEditor(result)
+            title = _this.processTitle(title, url)
+            _this.replaceWith(fetchingTitle, title)
         })
     }
 
@@ -61,6 +64,36 @@ export class Paster {
             let lastCharIndex = vscode.window.activeTextEditor.document.lineAt(startLine).text.length;
             let position = new vscode.Position(startLine, lastCharIndex);
             editBuilder.insert(position, content);
+            vscode.window.activeTextEditor.document.save()
+        });
+    }
+
+    replaceWith(originalContent, newContent) {
+        let document = vscode.window.activeTextEditor.document
+        var range: vscode.Range;
+        var line: String;
+        for (var i = 0; i < document.lineCount; i++) {
+            line = document.lineAt(i).text
+
+            if (line.includes(originalContent)) {
+                range = document.lineAt(i).range
+                break
+            }
+        }
+
+        if (range == undefined) {
+            return
+        }
+
+        var start = new Position(range.start.line, line.indexOf(originalContent));
+        var end = new Position(range.start.line, range.start.character + originalContent.length);
+
+        var newRange = new Range(start, end);
+        var text = document.getText(newRange);
+        vscode.window.activeTextEditor.edit((editBuilder) => {
+            editBuilder.replace(newRange, newContent);
+        }).then(function (a) {
+            console.log(a)
         });
     }
 
